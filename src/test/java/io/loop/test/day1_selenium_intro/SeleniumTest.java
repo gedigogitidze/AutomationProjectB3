a1b929f62985f7bf9d9fc37f84b97daec5c41f44
gedigogitidze <164251359+gedigogitidze@users.noreply.github.com>
2024-06-02 15:10:59 -0400

    My first Commit

--- src/test/java/io/loop/test/day1_selenium_intro/SeleniumTest.java
+++ src/test/java/io/loop/test/day1_selenium_intro/SeleniumTest.java
@@ -1,7 +1,44 @@
 package io.loop.test.day1_selenium_intro;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        // Setting up the web driver
+
+        //if you use older version then you need dependency for webdrivermanager - boni garica
+
+        //create
+        WebDriver driver = new ChromeDriver();
+
+        //maximize the window
+        driver.manage().window().maximize();
+        //driver.manage().window().fullscreen();
+        //navigate to the web page
+        driver.get("https://www.loopcamp.io");
+        Thread.sleep(5000);
+        driver.navigate().to("https://www.etsy.com");
+
+        // navigate back
+        driver.navigate().back();
+
+
+        //navigate forward
+        driver.navigate().forward();
+
+        // refresh
+        driver.navigate().refresh();
+
+        //quit and close
+      //  driver.close();
+        driver.quit();
+
+
+
+
 
     }
+
+
 }
