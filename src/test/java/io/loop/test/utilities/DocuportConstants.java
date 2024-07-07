3facdff09dec37cfe6d296c076a77a43b980c1fe
gedigogitidze <164251359+gedigogitidze@users.noreply.github.com>
2024-07-07 14:53:05 -0400

    day7 tables

--- src/test/java/io/loop/test/utilities/DocuportConstants.java
+++ src/test/java/io/loop/test/utilities/DocuportConstants.java
@@ -2,11 +2,25 @@ package io.loop.test.utilities;
 
 public class DocuportConstants {
 
-    public static final String USERNAME_CLIENT = "b1g2_client@gmail.com";
+
     public static final String ERROR_MESSAGE_FOR_EMPTY_PASSWORD = "Please enter your password";
     public static final String LOGO_DOCUPORT = "Docuport";
     public static final String RESET_PASSWORD = "reset-password";
     public static final String RESET_PASSWORD_MESSAGE = "Enter the email address associated with your account";
     public static final String EMAIL_FOR_RESET_PASSWORD = "forgotpasswordg1@gmail.com";
     public static final String CONFIRMATION_MESSAGE = "We've sent you an email with a link to reset your password. Please check your email.";
+    public static final String PASSWORD = "Group3";
+
+    //username
+    public static final String USERNAME_CLIENT = "b1g3_client@gmail.com";
+    public static final String USERNAME_ADVISOR = "b1g3_advisor@gmail.com";
+    public static final String USERNAME_SUPERVISOR = "b1g3_supervisor@gmail.com";
+    public static final String USERNAME_EMPLOYEE = "b1g3_employee@gmail.com";
+
+    // roles
+    public static final String ADVISOR = "advisor";
+    public static final String CLIENT = "client";
+    public static final String SUPERVISOR = "supervisor";
+    public static final String EMPLOYEE = "employee";
+
 }
